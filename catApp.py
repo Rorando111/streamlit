ae374294f6ae76740d1dd9263ae7997e552c339b
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-17 03:07:39 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -9,7 +9,7 @@ import io
 # Load the model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    return tf.keras.models.load_model('/content/drive/MyDrive/Finals Dataset/cat_classifier.h5')
+    return tf.keras.models.load_model('cat_classifier.h5')
 
 model = load_model()
 
