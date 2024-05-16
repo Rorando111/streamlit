814114f7abcf0dee546c2cfb4e719fd2d88cbba8
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-17 03:26:14 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -14,8 +14,8 @@ CLASS_NAMES = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
 @st.cache_resource
 def load_model() -> tf.keras.Model:
     """Load the cat breed classifier model"""
-    # Load the model from the saved format
-    model = tf.keras.models.load_model('saved_cat_classifier', custom_objects={"BatchNormalization": tf.keras.layers.BatchNormalization})
+    custom_objects = {"BatchNormalization": tf.keras.layers.BatchNormalization}
+    model = tf.keras.models.load_model('cat_classifier.h5', custom_objects=custom_objects)
     return model
 
 def import_and_resize_image(image_data: bytes) -> Image:
