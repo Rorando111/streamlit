6a08e6c3886f2eb0666dac2c28d565c121133fcf
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-17 01:30:51 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,40 +1,36 @@
 import streamlit as st
 import tensorflow as tf
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('cat_classifier.h5')
-    return model
-
-model = load_model()
-
+  model=tf.keras.models.load_model('cat_classifier.h5')
+  return model
+model=load_model()
 st.write("""
-# Cat Breed Classifier
-""")
+# Cat Breed Classifier"""
+)
+file=st.file_uploader("Choose cat photo from device",type=["jpg","png"])
 
-file = st.file_uploader("Choose cat photo from device", type=["jpg", "png"])
-
-def import_and_predict(image_data, model):
-    size = (224, 224)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis,...]
-    prediction = model.predict(img_reshape)
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(224,224)
+    image=ImageOps.fit(image_data,size,Image.LANCZOS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
-
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+     class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
                    'British Shorthair', 'Egyptian Mau', 'Maine Coon',
                    'Norweigian forest', 'Persian', 'Ragdoll',
                    'Russian Blue', 'Siamese', 'Sphynx']
     top_prediction = np.argmax(prediction)
-    string = "OUTPUT : " + class_names[top_prediction]
+    string = "Predicted Breed : " + class_names[top_prediction]
     st.success(string)
